5089855d7dd9258fb7a02308eeb1337233803d25
99003746 <nitin.shetty1@ltts.com>
2021-03-12 05:18:22 +0530

    updated Program_004 using vs_code in windows

--- Program_003.py
+++ Program_003.py
@@ -1,4 +1,9 @@
-num1=7
-num2=0
-print(num1 / num2) #gives zero devision error
+try:
+    num1=7
+    num2=0
+    print(num1 / num2) #gives zero devision error
+    print("Done calculation")
+except ZeroDicvisionError:
+    print("An error occured")
+    print("due to zero division")
   
