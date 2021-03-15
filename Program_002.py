c375190e26ef41db0620afe9bbffc7618c92652e
99003746 <nitin.shetty1@ltts.com>
2021-03-15 11:56:06 +0530

    updated Program_004(exception) using pycharm in windows

--- Program_002.py
+++ Program_002.py
@@ -1,1 +1,13 @@
-print("hello world from pycharm")
+#print("hello world from pycharm")
+
+text = input().split()
+length = [len(x) for x in text]
+maximum = max(length)
+text_index = length.index(maximum)
+print(text[text_index])
+try:
+  print(10/0)
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
